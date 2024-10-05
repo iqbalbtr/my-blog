921294a4c040c64b673b713369964002d417c3a1
Iqbal Bahtiar <iqbalbahtiar045@gmail.com>
2024-10-05 23:04:46 +0700

    add style category and tag

--- src/components/card/Category.vue
+++ src/components/card/Category.vue
@@ -4,14 +4,14 @@ defineProps<{
 }>()
 defineEmits(['handleRefresh'])
 
-const route = useRoute()
+const blog = useBlog()
 
 </script>
 
 <template>
         <div  class="text-xl font-semibold pt-12 flex gap-2 md:gap-8">
             <span v-for="category in categories.slice(0,3)" class="font-normal" @click="$emit('handleRefresh', 'category', category)" >
-                <NuxtLink class="text-base sm:text-lg hover:bg-secondary duration-200 md:text-xl font-bold text-slate-200 px-3 py-1 rounded-md cursor-pointer" :class="route.query.category === category ? 'bg-secondary' : ''" >{{ category }}</NuxtLink>
+                <button class="text-base sm:text-lg hover:bg-secondary duration-200 md:text-xl font-bold text-slate-200 px-3 py-1 rounded-md cursor-pointer" :class="blog.pagging.query === category ? 'outline outline-indigo-900' : ''" >{{ category }}</button>
             </span>
         </div>
 </template>
--- src/components/card/Tag.vue
+++ src/components/card/Tag.vue
@@ -2,7 +2,9 @@
 defineProps<{
     tags: string[]
 }>()
-defineEmits(['handleRefresh'])
+defineEmits(['handleRefresh']);
+
+const blog = useBlog()
 
 const route = useRoute()
 
@@ -12,9 +14,9 @@ const route = useRoute()
     <div class="px-6 py-5 h-fit bg-secondary rounded-lg">
         <h1 class="text-lg font-semibold md:text-2xl border-l-4 border-tint pl-2">Tags</h1>
         <div class="flex flex-wrap gap-2 pt-6">
-            <span v-for="tag in tags" class="py-1 px-3 cursor-pointer text-xs sm:text-sm md:text-base hover:scale-95 transition-all active:scale-90 bg-tint rounded-md font-semibold">
-                <NuxtLink @click="$emit('handleRefresh', 'tags', tag)" class="text-sm">{{
-                    tag }}</NuxtLink>
+            <span v-for="tag in tags" :class="blog.pagging.query == tag && 'outline outline-offset-2 outline-purple-500'" class="py-1 px-3 cursor-pointer text-xs sm:text-sm md:text-base hover:scale-95 transition-all active:scale-90 bg-tint rounded-md font-semibold">
+                <button @click="$emit('handleRefresh', 'tags', tag)" class="text-sm">{{
+                    tag }}</button>
             </span>
         </div>
     </div>
